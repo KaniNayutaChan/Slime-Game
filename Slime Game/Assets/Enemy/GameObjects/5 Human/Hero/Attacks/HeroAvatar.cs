ee953f50d4fbba5181c65c07a80829fca789b282
Joshua Tan <jaykid1jaykid1@gmail.com>
2021-05-31 20:29:57 +0930

    added more polymorphism to hero clone

--- Slime Game/Assets/Enemy/GameObjects/5 Human/Hero/Attacks/HeroAvatar.cs
+++ Slime Game/Assets/Enemy/GameObjects/5 Human/Hero/Attacks/HeroAvatar.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroAvatar : BaseEnemyAttack
+public class HeroAvatar : BaseHeroAttack
 {
-    Hero hero;
-
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        base.OnStateEnter(animator, stateInfo, layerIndex);
-
-        hero = animator.GetComponent<Hero>();
-    }
-
     protected override void UseAttack()
     {
         hero.avatar = Instantiate(hero.avatarPrefab);
--- Slime Game/Assets/Enemy/GameObjects/5 Human/Hero/BaseHeroAttack.cs
+++ Slime Game/Assets/Enemy/GameObjects/5 Human/Hero/BaseHeroAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BaseHeroAttack : BaseEnemyAttack
 {
-    Hero hero;
+    protected Hero hero;
 
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
